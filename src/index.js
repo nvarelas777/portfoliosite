8cd8830604487a9524a1b031d0cd829b1bbeb1ba
Nick Varelas <nick.varelas@dealersync.com>
2019-10-08 14:41:20 -0700

    mobile menu added

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@
 var navbar = document.getElementById("nav-bar");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+var navExpand = document.getElementById("nav-bar-expand");
+
+navExpand.addEventListener('click', function() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
   } else {
-    navbar.classList.remove("sticky");
+    x.style.display = "block";
   }
-}
+});
 
 function on() {
   document.getElementById("overlay").style.display = "block";
